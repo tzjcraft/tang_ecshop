0a5cff8047daf7691d860bfb885ebb726a5cebdd
Zugor Tang <tangzj@shinetechchina.com>
2013-12-25 02:54:04 +0800

    API: Goods sales

---
+++ tt/controller/home/goods_sales.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ *
+ *       _/_/_/                      _/        _/_/_/_/_/
+ *    _/          _/_/      _/_/    _/  _/          _/      _/_/      _/_/
+ *   _/  _/_/  _/_/_/_/  _/_/_/_/  _/_/          _/      _/    _/  _/    _/
+ *  _/    _/  _/        _/        _/  _/      _/        _/    _/  _/    _/
+ *   _/_/_/    _/_/_/    _/_/_/  _/    _/  _/_/_/_/_/    _/_/      _/_/
+ *
+ *
+ *  Copyright 2013-2014, Geek Zoo Studio
+ *  http://www.ecmobile.cn/license.html
+ *
+ *  HQ China:
+ *    2319 Est.Tower Van Palace
+ *    No.2 Guandongdian South Street
+ *    Beijing , China
+ *
+ *  U.S. Office:
+ *    One Park Place, Elmira College, NY, 14901, USA
+ *
+ *  QQ Group:   329673575
+ *  BBS:        bbs.ecmobile.cn
+ *  Fax:        +86-10-6561-5510
+ *  Mail:       info@geek-zoo.com
+ */
+
+require(EC_PATH . '/includes/init.php');
+
+include_once(EC_PATH . '/includes/lib_goods.php');
+
+$type = _POST('type');
+$recommend_goods = get_recommend_goods($type);
+GZ_Api::outPut($recommend_goods);
